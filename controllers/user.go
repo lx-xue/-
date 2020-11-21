7f0c026f34ed9648bb859f86a7aaaeffdcce2187
liuxue123456 <18749817085@163.com>
2020-11-21 15:57:48 +0800

    1

--- controllers/user.go
+++ controllers/user.go
@@ -10,9 +10,10 @@ type UserController struct {
 }
 
 func (user *UserController) Index() {
+	numbers := make([]int, 3, 5)
 	id := user.Ctx.Input.Param(":id")
 	name := user.Ctx.Input.Param(":name")
-
+	user.Data["numbers"] = numbers //切片类型变量
 	//输出相应
 	mysqluser := "数据库用户名" + beego.AppConfig.String("mysqluser") + "\n"
 	mysqlpass := "数据库用户密码" + beego.AppConfig.String("mysqlpass") + "\n"
@@ -31,5 +32,10 @@ func (user *UserController) AddUser() {
 	sex := user.GetString("sex")
 	height := user.GetString("height")
 	weight := user.GetString("weight")
+	var slice []float64 = make([]float64, 5, 10) //创建一个切片
+	var slice1 [5]int = [...]int{1, 2, 3, 4, 5}  //创建一个切片,并初始化
+	arr := slice1[1:3]
+	user.Data["slice"] = slice
+	user.Data["slice1"] = arr
 	user.Ctx.WriteString("post表单提交成功:\n" + sex + height + weight)
 }
